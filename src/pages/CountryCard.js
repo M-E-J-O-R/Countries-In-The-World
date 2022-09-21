f1c98778a3adf9ad2f8a0b69b2c295b10930b24c
Mejor <amujobabatunde7@gmail.com>
2022-09-21 18:39:34 +0100

    changed the filter to map

--- src/pages/CountryCard.js
+++ src/pages/CountryCard.js
@@ -23,43 +23,53 @@ const CountryCard = () => {
 function Card({ data }) {
     const { userInput } = useContext(CountryContext);
     let navigate = useNavigate()
-    function filteredName(data) {
+    // function filteredName(data) {
 
-        if (data.name.common.toLowerCase().includes(userInput.toLowerCase())) {
-            return data;
-        }
-        else if (data === '') {
-            return data;
-        }
-    }
+    //     if (data.name.common.toLowerCase().includes(userInput.toLowerCase())) {
+    //         return data;
+    //     }
+    //     else if (data === '') {
+    //         return data;
+    //     }
+    // }
 
 
 
     return (
 
         <>
+
             <div className="country-card-container">
 
-                {data.filter(filteredName).map((data, key) => {
 
 
-                    return (<div key={key} onClick={() => navigate(`/countrypage/${key}`)} className="country-card">
-                        <div className="country-flags" style={{
-                            backgroundImage: `url(${data.flags.svg})`
-                        }}></div>
+                {data.map((datas, index) => {
+                
+                        if (datas.name.common.toLowerCase().includes(userInput.toLowerCase()) || userInput === '') {
+                            return (<div key={index} onClick={() => navigate(`/countrypage/${index}`)} className="country-card">
+                                <div className="country-flags" style={{
+                                    backgroundImage: `url(${datas.flags.svg})`
+                                }}></div>
 
-                        <div className='country-details'>
-                            <h4 className='country-name'>{data.name.official}</h4>
+                                <div className='country-details'>
+                                    <h4 className='country-name'>{datas.name.official}</h4>
 
-                            <p><b>Population:</b>  {data.population.toLocaleString()}</p>
-                            <p> <b>Region:</b> {data.region}</p>
-                            <p> <b>Capital:</b> {data.capital}</p>
-                        </div>
-                    </div>
+                                    <p><b>Population:</b>  {datas.population}</p>
+                                    <p> <b>Region:</b> {datas.region}</p>
+                                    <p> <b>Capital:</b> {datas.capital}</p>
+                                </div>
+                            </div>
 
-                    );
+                            )
+                        }
+
+                   
+
 
                 })}
+
+
+
             </div>
 
 
